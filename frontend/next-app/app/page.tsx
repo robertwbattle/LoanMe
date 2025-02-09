17b9cac7a6c69fae16bc3020d968122435d686b9
kazisean <keh8423@nyu.edu>
2025-02-09 11:05:33 -0500

    savE

--- frontend/next-app/app/page.tsx
+++ frontend/next-app/app/page.tsx
@@ -26,15 +26,15 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-8">
       <div className="z-10 w-full max-w-xl px-5 xl:px-0">
-        <a
-          href="#"
-          className="mx-auto mb-5 flex max-w-fit animate-fade-up items-center justify-center space-x-2 overflow-hidden rounded-full bg-blue-100 px-7 py-2 transition-colors hover:bg-blue-200"
-        >
-          <Twitter className="h-5 w-5 text-[#1d9bf0]" />
-          <p className="text-sm font-semibold text-[#1d9bf0]" href="https://www.google.com/">
-            Welcome to LoanMe
-          </p>
-        </a>
+      <a target="_blank"
+        href="https://twitter.com/intent/tweet?text=LoanMe%3A%20Best%20P2P%20microloan%20platform%20in%20the%20web%20http://loanme.nyc/"
+        className="mx-auto mb-5 flex max-w-fit animate-fade-up items-center justify-center space-x-2 overflow-hidden rounded-full bg-blue-100 px-7 py-2 transition-colors hover:bg-blue-200"
+      >
+        <Twitter className="h-5 w-5 text-[#1d9bf0]" />
+        <p className="text-sm font-semibold text-[#1d9bf0]">
+          Welcome to LoanMe
+        </p>
+      </a>
         <h1
           className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent md:text-7xl md:leading-[5rem]"
           style={{ animationDelay: "0.15s" }}
